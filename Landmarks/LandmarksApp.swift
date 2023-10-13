0f4fd8c499b6ed2e3649573a456344f6ecc49613
Masab Ejaz <138084661+onlymasab@users.noreply.github.com>
2023-10-13 16:24:46 +0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Masab Ejaz on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
